e1afbe98f06823dbad295c295c1518a1f630e251
Lakshmi S Acharya <lakshmi.sacharya23@gmail.com>
2026-07-27 06:48:13 +0530

    Added Week3 Spring Learn hands-on

---
+++ Week2/orm-learn/src/main/resources/CountryData.sql
@@ -0,0 +1,3 @@
+insert into country(code, name) values ('IND', 'India');
+insert into country(code, name) values ('USA', 'United States');
+insert into country(code, name) values ('GBR', 'United Kingdom');
